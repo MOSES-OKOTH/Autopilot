2cb402aaf9462f56a1b254f921038aff9e693d09
MOSES-OKOTH <73462281+MOSES-OKOTH@users.noreply.github.com>
2025-05-17 15:43:34 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,33 @@
+(function(){
+    // Alerting the bot is running
+    console.log("BM Bot is running!");
+
+    // Declaring the cases
+    const cases = [
+
+    ];
+
+    // Declaring the target elements
+    const spans = document.querySelectorAll('span');
+
+    // Looping through the spans
+    spans.forEach((span)=>{
+        //Detecting the click
+        span.addEventListener('dblclick', function(){
+            // Getting the ID
+            const id = span.textContent.trim();
+
+            console.log("ID: " + id);
+
+            // Checking if the ID is in the cases
+            if(cases.includes(id)){
+                // Alerting the user
+                span.innerHTML += `<br><p style="color: white; background: purple; padding: 5px 16px; border-radius: 6px; font-size: 14px; font-weight: 500;">BM CASE!!! Refer with provided materials</p>`;
+            } else {
+                // Alerting the user
+                span.innerHTML += `<br><p style="color: white; background: blue; padding: 5px 16px; border-radius: 6px; font-size: 14px; font-weight: 500;">Not a BM Case. Handle cautiously</p>`;
+            }
+        });
+    });
+
+})();
