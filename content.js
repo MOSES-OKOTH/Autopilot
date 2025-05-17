0c9dc1b0084aadc43bd41cdfc9d54790280ec4ae
MOSES OKOTH <mosesokoth164@gmail.com>
2025-05-17 19:49:05 +0300

    Added icons and little optimization

--- content.js
+++ content.js
@@ -4,7 +4,7 @@
 
     // Declaring the cases
     const cases = [
-
+        
     ];
 
     // Declaring the target elements
@@ -22,10 +22,10 @@
             // Checking if the ID is in the cases
             if(cases.includes(id)){
                 // Alerting the user
-                span.innerHTML += `<br><p style="color: white; background: purple; padding: 5px 16px; border-radius: 6px; font-size: 14px; font-weight: 500;">BM CASE!!! Refer with provided materials</p>`;
+                span.innerHTML += `<br><p style="color: white; background: purple; padding: 5px 16px; border-radius: 6px; font-size: 14px; font-weight: bold;">BM CASE!!! Refer with provided materials</p>`;
             } else {
                 // Alerting the user
-                span.innerHTML += `<br><p style="color: white; background: blue; padding: 5px 16px; border-radius: 6px; font-size: 14px; font-weight: 500;">Not a BM Case. Handle cautiously</p>`;
+                span.innerHTML += `<br><p style="color: white; background: blue; padding: 5px 16px; border-radius: 6px; font-size: 14px; font-weight: bold;">Not a BM Case. Handle cautiously</p>`;
             }
         });
     });
